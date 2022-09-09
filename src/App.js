c4f199f33b170e74f8552fb43eeaf66391fcfea6
yulun wang <yulunwang@yuluns-MacBook-Air.local>
2022-09-08 21:18:42 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Web Development Fall 2022
         </p>
         <a
           className="App-link"
