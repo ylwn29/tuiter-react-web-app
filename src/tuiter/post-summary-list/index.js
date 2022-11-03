6f0d31ad9f5f8b8bac7e25d49ea25d229975e295
ylwn29 <ylwn29@gmail.com>
2022-11-03 13:51:59 -0700

    finish a7 lab part

--- src/tuiter/post-summary-list/index.js
+++ src/tuiter/post-summary-list/index.js
@@ -1,8 +1,11 @@
 import React from "react";
-import postsArray from './posts.json';
+// import postsArray from './posts.json';
 import PostSummaryItem from "./post-summary-item.js";
+import {useSelector} from "react-redux";
 
 const PostSummaryList = () => {
+    const postsArray = useSelector(state => state.tuits)
+
     return(
         <ul className="list-group">
             {
---
+++ src/tuiter/reducers/tuits-reducer.js
@@ -0,0 +1,10 @@
+import { createSlice } from "@reduxjs/toolkit";
+import tuits from '../data/tuits.json';
+
+const tuitsSlice = createSlice({
+    name: 'tuits',
+    initialState: tuits
+});
+
+export default tuitsSlice.reducer;
+
